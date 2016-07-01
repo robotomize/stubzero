41f60280d7a8d32067bd12f58666e9dfc9451bab
robotomize <robotomize@sakh.com>
2016-07-01 14:21:33 +1100

    methods of generator to output the collections classes

--- src/stubzero/Generator.php
+++ src/stubzero/Generator.php
@@ -11,12 +11,38 @@ namespace stubzero;
  */
 class Generator
 {
+    public function code($path)
+    {
+        $crawler = new ClassCrawler($path);
+        $crawler->start();
+    }
+
+    /**
+     * @param array $classes
+     *
+     * @return array
+     */
+    public static function generateSmartCollection(array $classes)
+    {
+        return self::pullToCollection($classes, 'generateSmart');
+    }
+
+    /**
+     * @param array $classes
+     *
+     * @return array
+     */
+    public static function generateQuickCollection(array $classes)
+    {
+        return self::pullToCollection($classes, 'generateQuick');
+    }
+
     /**
      * @param $className
      * @return mixed
      * @throws Exception\StubZeroException
      */
-    public static function createDeep($className)
+    public static function generateSmart($className)
     {
         $creator = new Creator($className);
         $creator->setType(Creator::LEXICAL_TYPE);
@@ -30,12 +56,28 @@ class Generator
      * @return mixed
      * @throws Exception\StubZeroException
      */
-    public static function createQuick($className)
+    public static function generateQuick($className)
     {
         $creator = new Creator($className);
         $creator->setType(Creator::VAR_TYPE);
         $creator->start();
 
         return $creator->getFoundModel();
+    }
+
+    /**
+     * @param array $classes
+     * @param       $methodName
+     *
+     * @return array
+     */
+    private static function pullToCollection(array $classes, $methodName)
+    {
+        $objectCollection = [];
+
+        foreach ($classes as $name) {
+            $objectCollection[] = $methodName($name);
+        }
+        return $objectCollection;
     }
 }
